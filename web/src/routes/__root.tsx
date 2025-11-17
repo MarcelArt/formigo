4fd57221f8162eb62d403e55806f683f210e23f7
MarcelArt <badman.islemon@gmail.com>
2025-11-17 22:35:41 +0700

    chore - remove unnecessary codes

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -6,7 +6,6 @@ import { AppSidebar } from '@/components/app-sidebar';
 import { Separator } from '@/components/ui/separator';
 import useAuth from '@/hooks/useAuth';
 import { Toaster } from '@/components/ui/sonner';
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '@/components/ui/breadcrumb';
 import { BreadcrumbComponent } from '@/components/breadcrumb-component';
 
 export const Route = createRootRoute({
@@ -42,17 +41,6 @@ export const Route = createRootRoute({
 					<header className="bg-background sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b px-4">
 						<SidebarTrigger className="-ml-1" />
 						<Separator orientation="vertical" className="mr-2 h-4" />
-						{/* <Breadcrumb>
-							<BreadcrumbList>
-								<BreadcrumbItem className="hidden md:block">
-									<BreadcrumbLink href="#">Building Your Application</BreadcrumbLink>
-								</BreadcrumbItem>
-								<BreadcrumbSeparator className="hidden md:block" />
-								<BreadcrumbItem>
-									<BreadcrumbPage>Data Fetching</BreadcrumbPage>
-								</BreadcrumbItem>
-							</BreadcrumbList>
-						</Breadcrumb> */}
 						<BreadcrumbComponent/>
 					</header>
 					<Outlet />
